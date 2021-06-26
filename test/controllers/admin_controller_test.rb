f1d8cc0dc687cd75ba513a64dd4620c4b9202bd3
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2021-06-26 18:43:56 +0900

    admin_controller_test、chat_controller_testに日本語でテストの下書き

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class AdminControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  # admin機能が使えるuserがuser_indexやowner_indexの画面に遷移することができるか
+  # admin機能が使えないuserの場合、root_pathにredirectされているか
+  # set_userやset_shopがbefore_loginで行われているか
+  # admin機能が使えるuserの場合、user_update、user_destroyができるか
+
 end
--- test/controllers/chats_controller_test.rb
+++ test/controllers/chats_controller_test.rb
@@ -2,4 +2,7 @@ require 'test_helper'
 
 class ChatsControllerTest < ActionDispatch::IntegrationTest
   
+  # before_actionでset_chatなどが正しく行われいるか
+  # showやshow_for_ownerアクションの際に、if文による条件分岐ができているか
+
 end
