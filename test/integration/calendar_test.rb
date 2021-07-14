1c78ff33abe10dac9e65ba2e47652a46da1b0fca
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2021-07-14 21:15:36 +0900

    calendarのテスト追加

--- test/integration/calendar_test.rb
+++ test/integration/calendar_test.rb
@@ -3,16 +3,23 @@ require 'test_helper'
 class ShopReservationFrameTest < ActionDispatch::IntegrationTest
   def setup
     @shop = shops(:sample_shop)
+    @available = @shop.availables.create(rent_date: "2021-07-14", start_time: "12:00:00")
   end
 
-  # test "予約して、Calendarテーブルの数が増えているか" do
-  #   assert_difference 'Calendar.count', 1 do
-  #     get c_new_path(@shop)
-  #     post c_confirm_path(@shop), params: { calendar: { rent_date: "2021-04-05, 12:00:00"} }
-  #     follow_redirect!
-  #     post c_create_path(@shop), params: { calendar: { candidate_days: @reservation_days} }
-  #   end
-  # end
+  test "予約枠を開放" do
+    assert_difference 'Available.count', 1 do
+      get s_res_path(@shop)
+      post s_set_res_path(@shop), params: { rent_date: "2021-07-14", start_time: "13", end_time: "14"}
+    end
+  end
+
+  test "予約する" do
+    assert_difference 'Calendar.count', 1 do
+      get c_new_path(@shop)
+      get c_confirm_path(@shop), params: { calendar: { rent_date: ["2021-07-14, 12:00:00"]} }
+      post c_create_path(@shop), params: { calendar: { candidate_days: ["2021-07-14 12:00:00"]} }
+    end
+  end
 
   # 複数日予約して、Calendarテーブルの数が増えているか
   # 過去日付を選択した際にエラーとして戻されるか
