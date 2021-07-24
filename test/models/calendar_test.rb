49ffb415745628a103a8e69d41bc1972e222914c
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2021-07-24 16:44:05 +0900

    calendarのmodelテスト追記

--- test/models/calendar_test.rb
+++ test/models/calendar_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class CalendarTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @shop = shops(:sample_shop)
+    @user = users(:ryoji)
+  end
+
+  test "shopに基づかないCalendarテーブルは作成されない" do
+    calendar = Calendar.new(user_id: @user.id, rent_date: "2021.07.21", start_time: "12:00:00")
+    assert_not calendar.save
+  end
+
+  test "userに基づかないCalendarテーブルは作成されない" do
+    calendar = Calendar.new(shop_id: @shop.id, rent_date: "2021.07.21", start_time: "12:00:00")
+    assert_not calendar.save
+  end
+
+  test "rent_dateのないCaelendarテーブルは作成されない" do
+    calendar = Calendar.new(shop_id: @shop.id, user_id: @user.id, start_time: "12:00:00")
+    assert_not calendar.save
+  end
+
+  test "shop、userに基いていて、rent_dateに値があればCalendarテーブルを作成できる" do
+    calendar = Calendar.new(shop_id: @shop.id, user_id: @user.id, rent_date: "2021.07.21", start_time: "12:00:00")
+    assert calendar.save
+  end
 end
