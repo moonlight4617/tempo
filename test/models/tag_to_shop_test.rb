0d3675f2de06bf802e65231cf72a7476469b86dc
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2021-08-23 23:56:53 +0900

    tag_testのテストコード追記

--- test/models/tag_to_shop_test.rb
+++ test/models/tag_to_shop_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class TagToShopTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+
+  # test "tagモデルに基づいていなければtag_to_shopは生成されない" do
+  #   Tag.create(name: "タグネーム")
+  #   tag = Tag.new(name: "タグネーム")
+  #   assert_not tag.valid?
   # end
+
+  # shopモデルに基づいていなければtag_to_shopは生成されない
+  # 同一のshop_idにおいて、tag_idは一意でなければいけない
+  # tag,shopモデルに基づいていて、shop_id,tag_idが一意であればtag_to_shopは生成できる
+
 end
