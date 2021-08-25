05e9a3e930241b2405fbbaafc7f3217328c371c2
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2021-08-25 23:00:28 +0900

    tag_to_shopモデルのテスト追記

--- test/models/tag_to_shop_test.rb
+++ test/models/tag_to_shop_test.rb
@@ -2,14 +2,32 @@ require 'test_helper'
 
 class TagToShopTest < ActiveSupport::TestCase
 
-  # test "tagモデルに基づいていなければtag_to_shopは生成されない" do
-  #   Tag.create(name: "タグネーム")
-  #   tag = Tag.new(name: "タグネーム")
-  #   assert_not tag.valid?
-  # end
+  def setup
+    @shop = shops(:sample_shop)
+  end
 
-  # shopモデルに基づいていなければtag_to_shopは生成されない
-  # 同一のshop_idにおいて、tag_idは一意でなければいけない
-  # tag,shopモデルに基づいていて、shop_id,tag_idが一意であればtag_to_shopは生成できる
+  test "tagモデルに基づいていなければtag_to_shopは生成されない" do
+    tag_to_shop = TagToShop.create(shop_id: @shop.id)
+    assert_not tag_to_shop.valid?
+  end
+
+  test "shopモデルに基づいていなければtag_to_shopは生成されない" do
+    tag = Tag.create(name: "テスト")
+    tag_to_shop = TagToShop.create(tag_id: tag.id)
+    assert_not tag_to_shop.valid?
+  end
+
+  test "同一のshop_idにおいて、tag_idは一意でなければいけない" do
+    tag = Tag.create(name: "テスト")
+    TagToShop.create(tag_id: tag.id, shop_id: @shop.id)
+    tag_to_shop = TagToShop.create(tag_id: tag.id, shop_id: @shop.id)
+    assert_not tag_to_shop.valid?
+  end
+
+  test "tag,shopモデルに基づいていて、shop_id,tag_idが一意であればtag_to_shopは生成できる" do
+    tag = Tag.create(name: "テスト")
+    tag_to_shop = TagToShop.create(tag_id: tag.id, shop_id: @shop.id)
+    assert tag_to_shop.valid?
+  end
 
 end
